d91935b6e0f27834bb964c2c12ef8669711998ce
Sumit Ganjave <ganjave_s@India.XoriantCorp.com>
2022-12-01 22:03:40 +0530

    updated image path

--- src/app/screenshot/screenshot.component.ts
+++ src/app/screenshot/screenshot.component.ts
@@ -72,32 +72,32 @@ export class ScreenshotComponent implements OnInit {
   };
   slidesStore = [
     {
-      src: './assets/showcase1.jpeg',
+      src: 'assets/showcase1.jpeg',
       alt: 'alt1',
       title: 'title1',
     },
     {
-      src: './assets/showcase2.jpeg',
+      src: 'assets/showcase2.jpeg',
       alt: 'alt2',
       title: 'title2',
     },
     {
-      src: './assets/showcase3.jpeg',
+      src: 'assets/showcase3.jpeg',
       alt: 'alt3',
       title: 'title1',
     },
     {
-      src: './assets/showcase4.jpeg',
+      src: 'assets/showcase4.jpeg',
       alt: 'alt4',
       title: 'title1',
     },
     {
-      src: './assets/showcase5.jpeg',
+      src: 'assets/showcase5.jpeg',
       alt: 'alt5',
       title: 'title1',
     },
     {
-      src: './assets/showcase6.jpeg',
+      src: 'assets/showcase6.jpeg',
       alt: 'alt6',
       title: 'title1',
     },
@@ -105,28 +105,28 @@ export class ScreenshotComponent implements OnInit {
 
   testimony = [
     {
-      src: './assets/testimonial/1479905563-testi-03.jpg',
+      src: 'assets/testimonial/1479905563-testi-03.jpg',
       name: 'Alex Rezvova',
       city: '',
       comment: 'Very excellent application and the fares are accurate',
       title: 'test1',
     },
     {
-      src: './assets/testimonial/1479989558-test-04.jpg',
+      src: 'assets/testimonial/1479989558-test-04.jpg',
       name: 'Albin Erlend',
       city: '',
       comment: 'At starting it was good and when I started using AXXYL',
       title: 'test2',
     },
     {
-      src: './assets/testimonial/1479905440-testi-02.jpg',
+      src: 'assets/testimonial/1479905440-testi-02.jpg',
       name: 'Jacob Kai',
       city: 'Indiana, USA',
       comment: 'Best app for booking one way cab. Nice billing system.',
       title: 'test3',
     },
     {
-      src: './assets/testimonial/1479989575-testi-01.jpg',
+      src: 'assets/testimonial/1479989575-testi-01.jpg',
       name: 'Alex Rezvova',
       city: 'New York, USA',
       comment: 'this is very easy to make book the cab so i like this app',
